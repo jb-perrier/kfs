ab4ca705cb3c1bf66d3067c80fc7338296622a58
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2023-01-21 20:21:47 +0100

    Cleanup

--- boot/start.asm
+++ boot/start.asm
@@ -1,15 +1,10 @@
-; Declare constants for the multiboot header.
 MBALIGN  equ  1 << 0            ; align loaded modules on page boundaries
 MEMINFO  equ  1 << 1            ; provide memory map
 MBFLAGS  equ  MBALIGN | MEMINFO ; this is the Multiboot 'flag' field
 MAGIC    equ  0x1BADB002        ; 'magic number' lets bootloader find the header
 CHECKSUM equ -(MAGIC + MBFLAGS)   ; checksum of above, to prove we are multiboot
  
-; Declare a multiboot header that marks the program as a kernel. These are magic
-; values that are documented in the multiboot standard. The bootloader will
-; search for this signature in the first 8 KiB of the kernel file, aligned at a
-; 32-bit boundary. The signature is in its own section so the header can be
-; forced to be within the first 8 KiB of the kernel file.
+; multiboot header
 section .multiboot
 align 4
 	dd MAGIC
@@ -36,7 +31,7 @@ _start:
 	extern kmain
 	call kmain
  
-	// infinite loop, since kmain returned while it should not
+	; infinite loop, since kmain returned while it should not
 	cli
 .hang:	hlt
 	jmp .hang

7fa331d83cb9f031df0f63f7640a3128d540239c
Azod <azod.contact@gmail.com>
2023-01-24 01:19:03 +0100

    add cursor pos

--- kernel/src/kernel.rs
+++ kernel/src/kernel.rs
@@ -2,6 +2,7 @@
 #![no_std]
 // we provide ur own !
 #![no_main]
+#![allow(unused)]
 
 mod libc;
 mod vga;
@@ -21,13 +22,15 @@ pub unsafe extern "C" fn kmain() -> ! {
     //vga::clear();
         // vga.write_str("Hello default colors");
     // vga.write_str_with_colors("That's cool with other colors !", &Colors::Green, &Colors::Black);
-    vga.write_str_with_colors("   ___             _        _      ___     ___  \n", &Colors::Green, &Colors::Black);
-    vga.write_str_with_colors("  | _ \\   __ _    | |_     (_)    / _ \\   / __|  \n", &Colors::Green, &Colors::Black);
-    vga.write_str_with_colors("  |   /  / _` |   |  _|    | |   | (_) |  \\__ \\  \n", &Colors::Green, &Colors::Black);
-    vga.write_str_with_colors("  |_|_\\  \\__,_|   _\\__|   _|_|_   \\___/   |___/ \n", &Colors::Green, &Colors::Black);
-    vga.write_str("_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n");
-    vga.write_str("\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n");
+    vga.write_str_with_colors("   ___             _        _      ___     ___  \n\r", &Colors::Green, &Colors::Black);
+    vga.write_str_with_colors("  | _ \\   __ _    | |_     (_)    / _ \\   / __|  \n\r", &Colors::Green, &Colors::Black);
+    vga.write_str_with_colors("  |   /  / _` |   |  _|    | |   | (_) |  \\__ \\  \n\r", &Colors::Green, &Colors::Black);
+    vga.write_str_with_colors("  |_|_\\  \\__,_|   _\\__|   _|_|_   \\___/   |___/ \n\r", &Colors::Green, &Colors::Black);
+    vga.write_str("_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n\r");
+    vga.write_str("\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n\r");
+    vga.write_str("\n\raaaaaa\nbbbbb");
 
+    vga.write_str("\n\r\n\r>");
     // KERNEL LOGIC
     loop {}
 }
--- kernel/src/vga.rs
+++ kernel/src/vga.rs
@@ -3,84 +3,111 @@ const VGA_ROWS: u32 = 24;
 const VGA_COLUMNS: u32 = 80;
 const VGA_MAX: u32 = VGA_ROWS * VGA_COLUMNS;
 const VGA_BUFFER: u32 = VGA_MAX * 2;
-
+use core::arch::asm;
 #[repr(u8)]
 #[derive(Copy, Clone)]
 pub enum Colors {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Purple = 5,
-    Brown = 6,
-    Gray = 7,
-    DarkGray = 8,
-    LightBlue = 9,
-    LightGreen = 10,
-    LightCyan = 11,
-    LightRed = 12,
-    LightPurple = 13,
-    Yellow = 14,
-    White = 15,
+	Black = 0,
+	Blue = 1,
+	Green = 2,
+	Cyan = 3,
+	Red = 4,
+	Purple = 5,
+	Brown = 6,
+	Gray = 7,
+	DarkGray = 8,
+	LightBlue = 9,
+	LightGreen = 10,
+	LightCyan = 11,
+	LightRed = 12,
+	LightPurple = 13,
+	Yellow = 14,
+	White = 15,
 }
 
+
+
 pub struct VGA {
-    index: isize,
+	index: isize,
 }
 
 impl VGA {
-    pub fn new() -> Self {
-        Self {
-            index: 0,
-        }
-    }
+	pub fn new() -> Self {
+		Self {
+			index: 0,
+		}
+		
+	}
+	
+	pub unsafe fn clear(&mut self) {
+		let mut i: isize = 0;
+		while i < VGA_BUFFER as isize {
+			let value = VGA_ADDR.offset(i).cast_mut();
+			*value = ' ' as u8;
+			i += 1;
+		}
+	}
+	
+	#[inline]
+	pub unsafe fn write(&mut self, c: char) {
+		self.write_with_colors(c, &Colors::White, &Colors::Black);
+	}
 
-    pub unsafe fn clear(&mut self) {
-        let mut i: isize = 0;
-        while i < VGA_BUFFER as isize {
-            let value = VGA_ADDR.offset(i).cast_mut();
-            *value = ' ' as u8;
-            i += 1;
-        }
-    }
-    
-    #[inline]
-    pub unsafe fn write(&mut self, c: char) {
-        self.write_with_colors(c, &Colors::White, &Colors::Black);
-    }
-    
-    #[inline]
-    pub unsafe fn write_with_colors(&mut self, c: char, fore_color: &Colors, back_color: &Colors) {
-        if c == '\n'{
-            let mut i: isize = self.index * 2;
-             while i == 0 || i % ((VGA_COLUMNS as isize) * 2) > 0 {
-            let value = VGA_ADDR.offset(i).cast_mut();
-            *value = ' ' as u8;
-            let col = VGA_ADDR.offset(i + 1).cast_mut();
-            *col = (Colors::Black as u8) << 5 | Colors::Black as u8;
-            i += 2;
-            self.index += 1;}
-        }
-        else {
-            let rindex = self.index * 2;
-            let cha = VGA_ADDR.offset(rindex).cast_mut();
-            *cha = c as u8;
-            let col = VGA_ADDR.offset(rindex + 1).cast_mut();
-            *col = (*back_color as u8) << 5 | *fore_color as u8;
-            self.index += 1;
-        }
-    }
+   #[inline]
+   pub unsafe fn outb(&mut self,port: u16, val: u16) {
+	   asm!("out dx, al", in("dx") port as u16, in("al") val as i8);
+   }
 
-    pub unsafe fn write_str(&mut self, str: &str) {
-        for c in str.chars() {
-            self.write(c);
-        }
-    }
+   #[inline]
+   pub unsafe fn inb(&mut self,port: u16) -> u8 {
+	  let return_val: u8;
+		asm!("in al, dx", in("dx") port as u16, out("al") return_val);
+	   return return_val;
+   }
+	
+	#[inline]
+	pub unsafe fn set_cursor_pos(&mut self,pos: u16) {
+		self.outb(0x3D4, 0x0F);
+		self.outb(0x3D5, (pos & 0xFF) as u16);
+		self.outb(0x3D4, 0x0E);
+		self.outb(0x3D5, ((pos >> 8) & 0xFF) as u16);
+	}
 
-    pub unsafe fn write_str_with_colors(&mut self, str: &str, fore_color: &Colors, back_color: &Colors) {
-        for c in str.chars() {
-            self.write_with_colors(c, fore_color, back_color);
-        }
-    }
+
+	#[inline]
+	pub unsafe fn write_with_colors(&mut self, c: char, fore_color: &Colors, back_color: &Colors) {
+		match c {
+			'\n' => {
+				self.index += VGA_COLUMNS as isize - (self.index % VGA_COLUMNS as isize);
+			},
+			'\t' => {
+				self.index += 4;
+			},
+			'\r' => {
+				self.index -= self.index % VGA_COLUMNS as isize;
+			},
+			c => {   
+				let rindex = self.index * 2;
+				let cha = VGA_ADDR.offset(rindex).cast_mut();
+				*cha = c as u8;
+				let col = VGA_ADDR.offset(rindex + 1).cast_mut();
+				*col = (*back_color as u8) << 5 | *fore_color as u8;
+				self.index += 1;
+			}
+		}
+		self.set_cursor_pos(self.index as u16);
+
+	}
+
+	pub unsafe fn write_str(&mut self, str: &str) {
+		for c in str.chars() {
+			self.write(c);
+		}
+	}
+
+	pub unsafe fn write_str_with_colors(&mut self, str: &str, fore_color: &Colors, back_color: &Colors) {
+		for c in str.chars() {
+			self.write_with_colors(c, fore_color, back_color);
+		}
+	}
 }

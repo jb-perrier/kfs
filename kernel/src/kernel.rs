f0fb14a49a1a88393a01cc3619c76586c772d34b
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2023-01-25 11:32:53 +0100

    Kernel interface

--- kernel/src/kernel.rs
+++ kernel/src/kernel.rs
@@ -16,31 +16,35 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// we should not mangle the name so the multiboot can find it at link time
-#[no_mangle]
-pub unsafe extern "C" fn kmain() -> ! {
-    let mut vga = VGA::new();
-    vga.clear();
-    
-    vga.write_str_with_colors("   ___             _        _      ___     ___  \n\r", &Colors::Green, &Colors::Black);
-    vga.write_str_with_colors("  | _ \\   __ _    | |_     (_)    / _ \\   / __|  \n\r", &Colors::Green, &Colors::Black);
-    vga.write_str_with_colors("  |   /  / _` |   |  _|    | |   | (_) |  \\__ \\  \n\r", &Colors::Green, &Colors::Black);
-    vga.write_str_with_colors("  |_|_\\  \\__,_|   _\\__|   _|_|_   \\___/   |___/ \n\r", &Colors::Green, &Colors::Black);
-    vga.write_str("_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n\r");
-    vga.write_str("\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n\r");
+pub struct Kernel {
 
-    vga.write_str("\n\r\n\r>");
-    vga.set_cursor_pos(8);
-    loop {
-        wait_and_shutdown();
+}
+
+impl Kernel {
+    pub unsafe fn start(&mut self) {
+        let mut vga = VGA::new();
+        vga.clear();
+    
+        vga.write_str_with_colors("   ___             _        _      ___     ___  \n\r", &Colors::Green, &Colors::Black);
+        vga.write_str_with_colors("  | _ \\   __ _    | |_     (_)    / _ \\   / __|  \n\r", &Colors::Green, &Colors::Black);
+        vga.write_str_with_colors("  |   /  / _` |   |  _|    | |   | (_) |  \\__ \\  \n\r", &Colors::Green, &Colors::Black);
+        vga.write_str_with_colors("  |_|_\\  \\__,_|   _\\__|   _|_|_   \\___/   |___/ \n\r", &Colors::Green, &Colors::Black);
+        vga.write_str("_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n\r");
+        vga.write_str("\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n\r");
+
+        vga.write_str("\n\r\n\r>");
+        vga.set_cursor_pos(8);
+        loop {}
+    }
+
+    pub unsafe fn shutdown() {
+        asm::shutdown();
     }
 }
 
-unsafe fn wait_and_shutdown() {
-    let mut i = 0;
-    while i < 50000000 {
-        asm::nop();
-        i += 1;
-    }
-    asm::shutdown();
+#[no_mangle]
+pub unsafe extern "C" fn kmain() -> ! {
+    let mut kernel = Kernel{};
+    kernel.start();
+    loop {}
 }

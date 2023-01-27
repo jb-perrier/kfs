0c56d2af9ac96c31973539c477ccfa96f94bd76d
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2023-01-27 14:58:03 +0100

    Improve start

--- kernel/src/kernel/mod.rs
+++ kernel/src/kernel/mod.rs
@@ -1,18 +1,24 @@
-pub mod libc;
-pub mod vga;
 pub mod asm;
-pub mod shell;
+pub mod libc;
 pub mod multiboot;
+pub mod shell;
+pub mod vga;
 
-use vga::*;
-use core::panic::PanicInfo;
 use self::multiboot::Multiboot;
+use core::panic::PanicInfo;
+use vga::*;
 
-pub static mut INSTANCE: Kernel = Kernel{};
+pub static mut INSTANCE: Kernel = Kernel {};
+
+macro_rules! infinite_loop {
+    () => {
+        loop {}
+    };
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    infinite_loop!()
 }
 
 // wait for allocator impl
@@ -22,9 +28,7 @@ pub trait Driver {
     fn destroy();
 }
 
-pub struct Kernel {
-
-}
+pub struct Kernel {}
 
 impl Kernel {
     pub unsafe fn start(&mut self, multiboot: *const Multiboot, magic: u32) {
@@ -34,28 +38,35 @@ impl Kernel {
         if magic != 0x2BADB002 {
             vga.write_str_with_colors("Unknown multiboot ! magic: ", &Colors::Red, &Colors::Black);
             vga.write_usize(magic as usize);
-            loop {}
+            infinite_loop!();
         }
 
         // Handle multiboot data
         vga.write_str("Multiboot found ! magic: ");
-        vga.write_usize((*multiboot).flags as usize);
+        vga.write_usize(magic as usize);
+        vga.write('\n');
+
+        vga.clear();
+        vga.set_index(0);
+
+        /*if ((*multiboot).flags >> 6 & 0x1) == 0 {
+            vga.write_str("Invalid memory map from multiboot info !\n");
+            infinite_loop!();
+        }*/
 
         // GDT
         // IDT
         // Paging
-        
-    
-        vga.write_str_with_colors("   ___             _        _      ___     ___  \n\r", &Colors::Green, &Colors::Black);
-        vga.write_str_with_colors("  | _ \\   __ _    | |_     (_)    / _ \\   / __|  \n\r", &Colors::Green, &Colors::Black);
-        vga.write_str_with_colors("  |   /  / _` |   |  _|    | |   | (_) |  \\__ \\  \n\r", &Colors::Green, &Colors::Black);
-        vga.write_str_with_colors("  |_|_\\  \\__,_|   _\\__|   _|_|_   \\___/   |___/ \n\r", &Colors::Green, &Colors::Black);
-        vga.write_str("_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n\r");
-        vga.write_str("\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n\r");
 
-        vga.write_str("\n\r\n\r>");
-        vga.set_cursor_pos(8);
-        loop {}
+        vga.write_str_with_colors(include_str!("./header_top"), &Colors::Green, &Colors::Black);
+        vga.write('\n');
+        vga.write_str(include_str!("./header_bottom"));
+
+        vga.write_str("\n\r\n\rkernel >");
+        let index = vga.get_index();
+        vga.set_cursor_pos(index);
+
+        infinite_loop!();
     }
 
     pub unsafe fn shutdown(&self) {

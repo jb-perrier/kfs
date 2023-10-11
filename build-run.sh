4057ee75af11609f96e945404264dbd873e7f347
Bycop <thebycop@gmail.com>
2023-10-11 12:52:44 +0200

    feat: Working linux version

--- build-run.sh
+++ build-run.sh
@@ -1,13 +1,13 @@
-./tools/install-fake-toolchain-rust-analyzer.bat
+sh ./tools/install-fake-toolchain-rust-analyzer.sh
 
 docker build ./tools -t kfs-builder
-OUTPUT=${docker ps -a -q -f "name=kfs-builder"}
+OUTPUT=$(docker ps -a -q -f "name=kfs-builder")
 if test -z "${OUTPUT}"
 then
     docker run -d --name kfs-builder -v ./:/home/kfs/ kfs-builder
 else
     docker container start kfs-builder
 fi
-docker exec -t "kfs-builder" sh /home/kfs/tools/docker-entrypoint.sh
+docker exec -t "kfs-builder" bash -c 'export PATH=/root/.cargo/bin:$PATH & make -f /home/kfs/Makefile'
 docker container stop kfs-builder
 qemu-system-i386 -s -cdrom ./bin/kfs.iso

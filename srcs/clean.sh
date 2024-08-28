85d135e1c641ba1b224a30c086d0513a6b3eeaa1
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2024-08-28 13:06:06 +0200

    Update clean.sh

--- srcs/clean.sh
+++ srcs/clean.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -d ./bin ]; then
-    rm -rf ./bin/*
+    rm -rf ./bin/
 fi
 
 if [ -d ./build ]; then
-    rm -rf ./build/*
+    rm -rf ./build/
 fi
 
 if [ -d ./kernel/target ]; then
-    rm -rf ./kernel/target/*
+    rm -rf ./kernel/target/
 fi
